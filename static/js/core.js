bf8876c2b213f209c86701975f49154eca4d7dff
Andrey Petrov <andrey.petrov@shazow.net>
2013-07-22 13:26:05 -0700

    Functioning firebase.

--- static/js/core.js
+++ static/js/core.js
@@ -1,15 +1,21 @@
 var SEARCH_API = 'http://hood.instantdomainsearch.com/services/quick?callback=?';
 var TLDS = ['biz', 'co', 'com', 'mobi', 'net', 'org'];
 
+var FIREBASE_API = 'https://wedomainsearch.firebaseio.com';
+var FIREBASE_BUCKET = 'demo';
+
 function clean_domain(q) {
     return q.split('.', 1)[0];
 }
 
 $(function() {
+    var fbase = new Firebase(FIREBASE_API + '/' + FIREBASE_BUCKET);
 
 
     $('#domain-query').submit(function() {
         var q = clean_domain($('#query', this).val());
+
+        fbase.child('history').push().set(q);
 
         $.getJSON(SEARCH_API, {'name': q}, function(data) {
             var ul = $('#result').empty();
@@ -20,11 +26,25 @@ $(function() {
                 if (data['tlds'].indexOf(tld) >= 0) {
                     c = 'not-available';
                 }
-                ul.append('<li class="'+ c +'">' + q + '.' + tld + '</li>');
+                var domain = q + '.' + tld;
+                ul.append('<li class="'+ c +'" data-domain="' + domain + '">' + domain + '</li>');
             }
         });
 
         return false;
+    });
+
+    $('#result').on('click', 'li.available', function() {
+        var domain = $(this).attr('data-domain');
+        fbase.child('best').push().set(domain);
+    });
+
+    fbase.child('history').limit(10).on('child_added', function(data) {
+        $('#history').prepend('<li>' + data.val() + '</li>');
+    });
+
+    fbase.child('best').on('child_added', function(data) {
+        $('#best').prepend('<li>' + data.val() + '</li>');
     });
 
 });
